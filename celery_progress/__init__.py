580516c02b830c8b59143b012b430f3d058d1324
GMcD <gary.macdonald@projectscapa.com>
2016-09-19 20:58:27 +0100

    Django 1.9 support.

--- celery_progress/__init__.py
+++ celery_progress/__init__.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 # from django.utils.module_loading import import_by_path
-from django.utils.module_loading.import_string
+from django.utils.module_loading import import_string
 
 BACKEND = getattr(settings, 'CELERY_PROGRESS_BACKEND',
                   'celery_progress.backends.CeleryBackend')
